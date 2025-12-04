284a43addd58ba5a389a13848a0da2095ee5026d
Jafary Mdegela <jaffarymdegela@gmail.com>
2025-12-04 18:14:42 +0300

    working-route-layout

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+import { useParams } from "next/navigation";
+
 function UserDetail() {
-  return <div>User</div>;
+  const { id } = useParams();
+  return (
+    <div>
+      <h1> Show detail number {id}</h1>
+    </div>
+  );
 }
 
 export default UserDetail;
--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        Comming from main layout
         {children}
       </body>
     </html>
